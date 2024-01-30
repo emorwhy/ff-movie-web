1aa9f82629ba3dad6a12f5a5ac4b26781edf9935
emorwhy y <emoryntucker@gmail.com>
2024-01-30 02:26:06 +0000

    fix proxy

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.__CONFIG__ = {
   // The URL for the CORS proxy, the URL must NOT end with a slash!
-  VITE_CORS_PROXY_URL: "https://proxy.movie-web.fortnitefajita.tk",
+  VITE_CORS_PROXY_URL: "https://movieweb-proxy.fortnite-fajita.workers.dev/",
 
   // The READ API key to access TMDB
   VITE_TMDB_READ_API_KEY: "CHANGeyJhdWQiOiIxMGViMTM5YTkyOTFkY2FhODZkNjcxMzk2NTgwZjcwMSIsInN1YiI6IjY1YjEzYmY5YTMxNDQwMDE3MjVhYzk0ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQEME",
