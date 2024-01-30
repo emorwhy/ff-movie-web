3759b6092866270f350390f6d7705525e1d8a679
emorwhy y <emoryntucker@gmail.com>
2024-01-30 02:37:05 +0000

    asdf

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ window.__CONFIG__ = {
   VITE_CORS_PROXY_URL: "https://movieweb-proxy.fortnite-fajita.workers.dev/",
 
   // The READ API key to access TMDB
-  VITE_TMDB_READ_API_KEY: "CHANGeyJhdWQiOiIxMGViMTM5YTkyOTFkY2FhODZkNjcxMzk2NTgwZjcwMSIsInN1YiI6IjY1YjEzYmY5YTMxNDQwMDE3MjVhYzk0ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQEME",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMGViMTM5YTkyOTFkY2FhODZkNjcxMzk2NTgwZjcwMSIsInN1YiI6IjY1YjEzYmY5YTMxNDQwMDE3MjVhYzk0ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._V_I-Gpchvq823gqplqo2uq9LfX5TfKNUGBihw0Hx2U",
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
   VITE_DMCA_EMAIL: null,
