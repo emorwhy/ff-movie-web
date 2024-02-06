ead73d65b566b4515caf300044292f7361c99254
emorwhy <76973043+emorwhy@users.noreply.github.com>
2024-02-06 10:01:08 -0600

    asdf

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.__CONFIG__ = {
   // The URL for the CORS proxy, the URL must NOT end with a slash!
-  VITE_CORS_PROXY_URL: "https://chic-gumdrop-c1e30f.netlify.app/",
+  VITE_CORS_PROXY_URL: "https://chic-gumdrop-c1e30f.netlify.app",
 
   // The READ API key to access TMDB
   VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMGViMTM5YTkyOTFkY2FhODZkNjcxMzk2NTgwZjcwMSIsInN1YiI6IjY1YjEzYmY5YTMxNDQwMDE3MjVhYzk0ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._V_I-Gpchvq823gqplqo2uq9LfX5TfKNUGBihw0Hx2U",
